da1491ca6c01b82a883c43fa0ab4f34318b76a4a
jeffk713 <52447909+jeffk713@users.noreply.github.com>
2020-06-15 23:38:52 -0700

    prepare for deployment

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('../config/db');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,5 +11,13 @@ app.use(express.json({ extended: false }));
 
 app.use('/user', require('./routes/user.route'));
 app.use('/task', require('./routes/task.route'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(exprss.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
